22854ee7cc59c3e36ff57b2a10fc3a6d5f018174
AmyXandra <amyabafor013@gmail.com>
2021-08-13 13:26:54 +0100

    adding redirect for netlify

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-// import {Router, Route} from 'react-router-dom';
 import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
 import Landing from './components/Landing';
 import Country from './components/Country';
